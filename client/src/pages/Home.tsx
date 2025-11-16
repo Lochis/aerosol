3cc21c2ab1c519e063632a447de869e904a534e8
Lucas <lukevdm64@gmail.com>
2025-11-16 17:08:39 -0500

    frontend:  createPost call

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,40 @@
+
 import Post from "../components/Post"
+import { authPost } from "../lib/auth";
 
 export default function Home() {
+    const data = "";
+
+    async function createPost() {
+        try {
+            const response = await authPost("http://localhost:3000/api/posts", data, {
+                headers: { "Content-Type": "application/json" },
+            });
+
+            console.log("Post creation successful:", response.data);
+        } catch (error) {
+            console.error("Error creating post:", error);
+        }
+
+
+
+    }
+
     const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return (
-        <div className="py-2">
-            {items.map((id) => (
-                <Post key={id} picture={id % 2 === 0} />
-            ))}
+        <div>
+            <div className="justify-center flex">
+                <textarea className="textarea hover:textarea-success" placeholder="Post..">
+
+
+                </textarea>
+            </div>
+
+            <div className="py-2">
+                {items.map((id) => (
+                    <Post key={id} picture={id % 2 === 0} />
+                ))}
+            </div>
         </div>
     )
 }
