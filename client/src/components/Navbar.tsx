ccb32d0b1534694e3bc63a107b90178bf452fca0
Lucas Vandermaarel <lukevdm64@gmail.com>
2025-11-25 09:54:44 -0500

    rebasing on main

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Avatar from "boring-avatars";
 import { useAuth } from "../lib/auth";
 import { useNavigate } from "react-router";
+import axios from "axios";
+import { useState } from "react";
 
 export default function NavBar() {
     const auth = useAuth();
@@ -10,42 +12,85 @@ export default function NavBar() {
         navigate("/auth");
     }
 
-    return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="mx-auto flex w-full">
-                <div className="flex-none">
-                    {/* Hamburger icon*/}
-                    <button className="btn btn-square btn-ghost">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
-                    </button>
-                </div>
-                <div className="flex-1">
-                    <a className="btn btn-ghost text-xl" href="/">Aerosol</a>
-                </div>
-                    <div className="dropdown dropdown-bottom dropdown-end relative">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar" >
-                            <div className="w-10 rounded-full" >
-                                <Avatar
-                                    size={40}
-                                    name="name"
-                                    variant="beam"
-                                    colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
-                                />
-                            </div>
-                        </div>
-                        <ul
-                            tabIndex={-1}
-                            className="right-0 menu dropdown-content bg-base-200 rounded-box z-1 mt-4 w-36 p-2 shadow-sm">
-                            <li>
-                                <a className="justify-between" href="/profile">
-                                    Profile
-                                </a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li><a onClick={handleLogout}>Logout</a></li>
-                        </ul>
-                    </div>
-            </div>
+  type ReducedUsers = {
+    id: string;
+    tag: string,
+    name: string,
+  };
+
+  const [searchUsers, setSearchUsers] = useState<ReducedUsers[]>([]);
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e: any) => {
+    setSearch(e.target.value);
+    getUsers(e.target.value);
+  }
+
+  const getUsers = async (search: string) => {
+    try {
+      const response = await axios.get(`${process.env.CLIENT_API_BASE}/users?search=${search}`, {
+        headers: { "Content-Type": "application/json" },
+      });
+
+      setSearchUsers(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching searchable users", error);
+    }
+  }
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="mx-auto flex w-full justify-between">
+        <div>
+          {/* Hamburger icon*/}
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+          </button>
+
+          <a className="btn btn-ghost text-xl" href="/">Aerosol</a>
         </div>
-    )
+        <div className="w-100 mx-auto">
+          <label className="input input-secondary">
+            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="text" className="grow" placeholder="Search" onChange={(e) => handleChange(e)} />
+          </label>
+        </div>
+        <div className="dropdown dropdown-bottom dropdown-end relative">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar" >
+            <div className="w-10 rounded-full" >
+              <Avatar
+                size={40}
+                name="name"
+                variant="beam"
+                colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={-1}
+            className="right-0 menu dropdown-content bg-base-200 rounded-box z-1 mt-4 w-36 p-2 shadow-sm">
+            <li>
+              <a className="justify-between" href="/profile">
+                Profile
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a onClick={handleLogout}>Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </div >
+  )
 }
--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -8,17 +8,15 @@ import type { Types } from 'mongoose';
 import config from '../config/config.ts';
 
 export async function login(req: JWTRequest, res: Response) {
-    console.log("Login request received:", req.body);
-    try {
-        let user = await User.findOne({ "email": req.body.email })
-        if (!user) {
-            return res.status(400).json({
-                "status": "error",
-                "error": {
-                    "code": "EMAIL_NOT_FOUND",
-                    "message": "No account found with that email."
-                }
-            });
+  console.log("Login request received:", req.body);
+  try {
+    let user = await User.findOne({ "email": req.body.email })
+    if (!user) {
+      return res.status(400).json({
+        "status": "error",
+        "error": {
+          "code": "EMAIL_NOT_FOUND",
+          "message": "No account found with that email."
         }
         // call user's authenticate method if available
         const authFn = (user as any).authenticate;
@@ -43,6 +41,36 @@ export async function login(req: JWTRequest, res: Response) {
             "error": { "message": err }
         });
     }
+    // call user's authenticate method if available
+    const authFn = (user as any).authenticate;
+    if (typeof authFn === "function") {
+      // use call to preserve method's this context - Mongoose instance methods
+      if (!authFn.call(user, req.body.password)) {
+        return res.status(401).json({
+          "status": "error",
+          "error": {
+            "code": "BAD_PASSWORD",
+            "message": "Email and password don't match."
+          }
+        });
+      }
+    }
+
+    const expiresIn = 900;
+    const accessToken = jwt.sign({ sub: user._id }, config.jwtSecret, { expiresIn });
+
+    return res.json({
+      accessToken: accessToken,
+      tokenType: "Bearer",
+      expiresIn: expiresIn,
+    })
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json({
+      "status": "error",
+      "error": { "message": err }
+    });
+  }
 }
 
 function authorizeUser(res: Response, sub: Types.ObjectId) {
@@ -64,56 +92,56 @@ function authorizeUser(res: Response, sub: Types.ObjectId) {
 
 export async function signup(req: JWTRequest, res: Response) {
 
-    try {
-        // normalize email and name to lowercase
-        const email = req.body.email.trim().toLowerCase();
+  try {
+    // normalize email and name to lowercase
+    const email = req.body.email.trim().toLowerCase();
 
-        // check if user with the same email already exists
-        const existingUser = await User.findOne({ email: email });
-        if (existingUser) {
-            return res.status(400).json({
-                status: "error",
-                error: {
-                    code: "DUPLICATE_EMAIL",
-                    message: "Email is already registered"
-                }
-            });
+    // check if user with the same email already exists
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res.status(400).json({
+        status: "error",
+        error: {
+          code: "DUPLICATE_EMAIL",
+          message: "Email is already registered"
         }
-
-        req.body.email = email;
-
-        const user = new User(req.body);
-        console.log("User object before save:", user);
-
-        // Validate explicitly to get validation errors before DB write
-        await user.validate().catch(validationErr => {
-            console.error("Validation errors:", validationErr);
-            throw { type: "VALIDATION", details: validationErr.errors };
-        });
-
-        // actual save
-        console.log("Attempting to save user to DB...");
-        await user.save();
-        console.log("User saved successfully:", user._id);
-
-
-        return res.status(201).json({
-            "status": "ok",
-            "data": { userId: user._id }
-        })
-    } catch (err) {
-        console.error(err)
-        return res.status(400).json({
-            "status": "error",
-            "error": { "message": err }
-        })
+      });
     }
+
+    req.body.email = email;
+
+    const user = new User(req.body);
+    console.log("User object before save:", user);
+
+    // Validate explicitly to get validation errors before DB write
+    await user.validate().catch(validationErr => {
+      console.error("Validation errors:", validationErr);
+      throw { type: "VALIDATION", details: validationErr.errors };
+    });
+
+    // actual save
+    console.log("Attempting to save user to DB...");
+    await user.save();
+    console.log("User saved successfully:", user._id);
+
+
+    return res.status(201).json({
+      "status": "ok",
+      "data": { userId: user._id }
+    })
+  } catch (err) {
+    console.error(err)
+    return res.status(400).json({
+      "status": "error",
+      "error": { "message": err }
+    })
+  }
 }
 
 export const requireSignin = expressjwt({
-    secret: config.jwtSecret,
-    algorithms: ["HS256"],
-    requestProperty: 'auth',
+  secret: config.jwtSecret,
+  algorithms: ["HS256"],
+  requestProperty: 'auth',
 })
 
 // Can this be merged into the refresh function?
