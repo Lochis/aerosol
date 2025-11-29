6eb5c222b54f8b06467b93182931fe0195a3da0f
Lucas <lukevdm64@gmail.com>
2025-11-29 18:40:38 -0500

    add the owner optional fields in IChannel

--- client/src/types/channel.types.ts
+++ client/src/types/channel.types.ts
@@ -3,6 +3,11 @@ import type { User } from "./user.types";
 export interface IChannel {
   _id?: string;
   name: string;
+  owner?: {
+    _id: User["_id"];
+    tag: User["tag"];
+    name: User["name"];
+  };
   type: "channel" | "dm";
   members: User["_id"][]; // only user IDs
 }
