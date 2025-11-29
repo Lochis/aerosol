14870ea96062d793f522ad3cb3b972e288e87162
Lucas <lukevdm64@gmail.com>
2025-11-28 21:07:54 -0500

    feat: once one person creates a dm, both people get it populated on their clients. Respectively, it is the other's tag.

--- client/src/components/Channel/Channel.tsx
+++ client/src/components/Channel/Channel.tsx
@@ -2,9 +2,11 @@ import Avatar from "boring-avatars";
 import type { Channel } from "./types/channel.types";
 export default function Channel({
   channel,
+  isUserOwner,
   onOpenChannel,
 }: {
   channel: Channel;
+  isUserOwner: boolean;
   onOpenChannel: (channel: Channel) => void;
 }) {
   const isDM = channel.type === "dm";
@@ -12,13 +14,13 @@ export default function Channel({
     <button className="btn btn-outline rounded-xl" onClick={() => onOpenChannel(channel)}>
       <Avatar
         size={32}
-        name={channel.members[0].name}
+        name={isUserOwner ? channel.members[0].name : channel.owner.tag}
         variant="beam"
         colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
       />
       <span className="text-base font-semibold">
         {isDM ? "@" : "#"}
-        {isDM ? channel.members[0].tag : channel.name}
+        {isDM ? (isUserOwner ? channel.members[0].tag : channel.owner.tag) : channel.name}
       </span>
     </button>
   );
