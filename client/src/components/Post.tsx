12605d454bbfd86d150ef82f76ff1cb92e4f2606
Lucas Vandermaarel <28302600+Lochis@users.noreply.github.com>
2025-12-04 14:10:08 -0500

    Syntax highlighting (#67)
    * Added code highlights
    
    * Code syntax using Shiki
    
    * feat: preview post on edit and when creating
    
    ---------
    
    Co-authored-by: Nick <nickwagner233@gmail.com>

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -5,24 +5,32 @@ import HeartIcon from "./icons/HeartIcon";
 import TrashIcon from "./icons/TrashIcon";
 import DOMPurify from "dompurify";
 import MarkdownIt from "markdown-it";
+import Shiki from "@shikijs/markdown-it";
 import type { Post as PostType } from "../types/post.types";
 import { useState } from "react";
 import { useAuth } from "../lib/auth";
 import { useToast } from "./Toast";
+import PreviewPostModal from "./PreviewPostModal";
 
-const md = new MarkdownIt({
+const md = MarkdownIt({
   breaks: true,
   linkify: true,
 });
+
+md.use(await Shiki({
+  theme: 'tokyo-night'
+}))
 
 export default function Post({
   post,
   onDelete,
   onEdit,
+  preview = false,
 }: {
   post: PostType;
   onDelete: (post: PostType) => void;
   onEdit: (post: PostType) => void;
+  preview?: boolean;
 }) {
   const auth = useAuth();
   const [editing, setEditing] = useState(false);
@@ -73,15 +81,17 @@ export default function Post({
           />
         )}
 
+        
+
         {/* Interaction buttons */}
         <div className="flex justify-between text-sm opacity-70">
-          <button className="btn btn-circle btn-ghost">
+          <button className="btn btn-circle btn-ghost" disabled={preview}>
             <CommentIcon />
           </button>
-          <button className="btn btn-circle btn-ghost">
+          <button className="btn btn-circle btn-ghost" disabled={preview}>
             <RepostIcon />
           </button>
-          <LikePost post={post} />
+          <LikePost post={post} preview={preview} />
         </div>
       </div>
     </div>
@@ -182,7 +192,21 @@ function EditContent({
         onChange={(e) => setContent(e.target.value)}
         disabled={pending}
       />
-      <div className="flex gap-2 mt-2 justify-end">
+
+      <div className="flex justify-between mt-2">
+        <PreviewPostModal postContent={content} modalID={`edit-preview-post-modal`} />
+        <button
+          className="btn btn-soft btn-sm justify-self-start"
+          onClick={() => {
+            const previewDialog = document.getElementById(`edit-preview-post-modal`) as HTMLDialogElement | null;
+            previewDialog?.showModal();
+          }}
+        >
+          Preview
+        </button>
+      
+      <div className="flex gap-2 justify-end">
+        
         <button
           className="btn btn-circle btn-ghost btn-sm opacity-70"
           onClick={handleDelete}
@@ -205,11 +229,12 @@ function EditContent({
           Cancel
         </button>
       </div>
+      </div>
     </div>
   );
 }
 
-function LikePost({ post }: { post: PostType }) {
+function LikePost({ post , preview = false}: { post: PostType ,preview?: boolean }) {
   const toast = useToast();
   const auth = useAuth();
   const [likes, setLikes] = useState(post.likes ?? 0);
@@ -238,7 +263,7 @@ function LikePost({ post }: { post: PostType }) {
         liked ? "text-red-500 scale-110" : "scale-100"
       }`}
       onClick={handleLike}
-      disabled={pending}
+      disabled={pending || preview}
       title={liked ? "Unlike" : "Like"}
     >
       <HeartIcon />
