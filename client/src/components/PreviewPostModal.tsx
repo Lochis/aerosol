6b124269a08c40f4b06a64c5f5a96c039e254ded
Lucas Vandermaarel <Lvande10@my.centennialcollege.ca>
2025-12-08 08:51:40 -0500

    minor(client): making post preview modal have pleasant spacing and able to overflow with scrollbar if post is large

--- client/src/components/PreviewPostModal.tsx
+++ client/src/components/PreviewPostModal.tsx
@@ -3,13 +3,15 @@ import Post from "./Post";
 export default function PreviewPostModal({ postContent, modalID }: { postContent: string, modalID: string }) {
   return (
     <dialog id={modalID} className="modal">
-      <div className="modal-box max-w-2xl overflow-hidden">
+      <div className="modal-box max-w-2xl">
         <form method="dialog">
           <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
             ✕
           </button>
-          <Post preview={true} post={{ _id: "preview", author: { _id: "preview", name: "preview", tag: "preview" }, content: postContent, createdAt: new Date().toISOString(), updatedAt: new Date().toISOString() }} onDelete={() => { }} onEdit={() => { }} />
         </form>
+        <div className="p-2">
+        <Post preview={true} post={{ _id: "preview", author: { _id: "preview", name: "preview", tag: "preview" }, content: postContent, createdAt: new Date().toISOString(), updatedAt: new Date().toISOString() }} onDelete={() => { }} onEdit={() => { }} />
+        </div>
       </div>
     </dialog>
   );
